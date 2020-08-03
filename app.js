0d1f44e20ae4de101a455ad2cb45cdc07a32a667
Jameison <69103872+Jameison-Tech@users.noreply.github.com>
2020-08-03 16:43:36 +1200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const cmdsArray = [
 bot.on("ready", () => {
     clear();
     console.log('______');
-    bot.user.setActivity('shoppy.gg/@JameisonKeys', { url: "https://github.com/alexlyee/massdm", type: 'PLAYING' })
+    bot.user.setActivity('shoppy.gg/@JameisonKeys', { url: "https://github.com/Jameison-Tech", type: 'Shopping at' })
         .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
         .catch(console.error);
     
@@ -55,9 +55,9 @@ catch (e) {
 function clear() {
     console.clear();
     console.log(figlet.textSync("MassDM v3.4.0b").green); // just in case it wasn't obvious, this is a beta
-    console.log("\n\nMass DM bot for Discord. \n Sends DMs to selected members of guild.\n  Forked and improved by Alex.");
-    console.log("\n     Don't forget to apply the proper permissions in Discord. Use https://github.com/alexlyee/massdm/issues to report issues. Suport server: https://discord.gg/mMWQaDx");
-    console.log("\n     After someone by the name of \"6669\" double-crossed me, \n(assuring me that I would be paid for working on the project, but once I did most of the work and proved it was fixed, blocked me) \n    I will be abandoning this project, please do not contact me to report bugs. I am aware that there are some. ");
+    console.log("\n\nMass DM bot for Discord. \n Sends DMs to selected members of guild.\n  .");
+    console.log("\n     Don't forget to apply the proper permissions in Discord. ");
+    console.log("\n     https://github.com/Jameison-Tech. ");
     console.log(`\nRandom send time set @ 0.01-${config.wait}s`);
     console.log(` Type  ${config.prefix}help  in a chat.\n\n`);
 }
@@ -85,9 +85,8 @@ NaN
 
 /*
 https://stackoverflow.com/questions/14274293/show-current-state-of-jenkins-build-on-github-repo
-https://docs.travis-ci.com/user/tutorial/   https://travis-ci.org/github/alexlyee/massdm/jobs/689535155
-https://app.snyk.io/org/alexlyee
-https://inch-ci.org/github/alexlyee/massdm?branch=master
+https://docs.travis-ci.com/user/tutorial/   
+
 https://hits.dwyl.com/
 https://david-dm.org/?success
 https://github.com/dwyl/goodparts#why
